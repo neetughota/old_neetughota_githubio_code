60b824930badf9c58cf569ddbb4e7c52d31582df
neetughota <neetu.ghota@gmail.com>
2019-07-28 19:14:30 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -22,8 +22,8 @@ d3.csv('data.csv',function (data) {
         '<div> <div class= "container"> <div class="row">'+
             '<label>' + playerName +  '</label></div>'+
          '<div class="row">'+
-            '<label style="border-right: 1px solid #fff;padding-right: 10px;">' + filteredData[0].Team + "|" + '</label>'+
-            '<label style="color: #fff;padding-left: 5px;">' +  filteredData[0].Position + '</label>'+
+            '<label style="border-right: 1px solid #fff;padding-right: 10px;">' + filteredData[0].Position   + " | " + '</label>'+
+            '<label style="padding-left: 5px;">' + filteredData[0].Team + '</label>'+
           '</div><div class="row"> <div class="col"> <label> Ht:  6-5 </label> </div> <div class="col"> <label> Wt:  220lbs </label> </div>'+
       '<div class="col"> <label> Prior: Boston Celtics </label> </div> <div class="col"> <label> Age:  22 </label> </div>'+   
       '</div></div></div>'+
