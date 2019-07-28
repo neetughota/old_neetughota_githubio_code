53fc57ccff2581fc4d5e17a1f0d546a8a762fa57
neetughota <neetu.ghota@gmail.com>
2019-07-28 14:24:16 -0400

    Update table.js

--- js/table.js
+++ js/table.js
@@ -237,52 +237,7 @@ d3.csv('data.csv',function (data) {
 })
 
 
-var tabulate5 = function (data,columns) {
- var margin = {top: 20, right: 10, bottom: 20, left: 10}
- var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
- var svg = d3.select("#stealspergame").append("svg")
- .attr("width", width + margin.left + margin.right)
- .attr("height", height + margin.top + margin.bottom) 
- .append("g")
- .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
- 
- 
- var forobj = svg.append("foreignObject")
- .attr("width", 300)
- .attr("height", 200)
- .append("xhtml:body")
- var table = forobj.append("table")
- var thead = table.append('thead')
- var tbody = table.append('tbody')
-	thead.append('tr')
-	.selectAll('th')
-	.data(columns)
-	.enter()
-	.append('th')
-	.text(function (d) { return d })
 
-	var rows = tbody.selectAll('tr')
-	.data(data)
-	.enter()
-	.append('tr')
-
-	var cells = rows.selectAll('td')
-	.data(function(row) {
-	return columns.map(function (column) {
-	return { column: column, value: row[column] }
-	})
- })
- .enter()
- .append('td')
- .text(function (d) { return d.value })
-
- return table;
-}
-
-d3.csv('data.csv',function (data) {
-	var columns = ['Name','Team','Position','DefRating']
- tabulate5(data,columns)
-})
 
 var tabulate6 = function (data,columns) {
  var margin = {top: 20, right: 10, bottom: 20, left: 10}
