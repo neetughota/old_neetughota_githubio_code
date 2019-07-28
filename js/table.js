f7704aac85951b612142782da9abe553f05bb252
neetughota <neetu.ghota@gmail.com>
2019-07-28 13:14:47 -0400

    Update table.js

--- js/table.js
+++ js/table.js
@@ -6,14 +6,7 @@ var tabulate = function (data,columns) {
  .attr("height", height + margin.top + margin.bottom) 
  .append("g")
  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
- var svgtext = svg.append("text")
- .attr("x",(width/2))
- .attr("y",0-(margin.top/4))
- .style("text-anchor","middle")
- .style("font-size","16px")
- .style("text-decoration","underline")
- .style("font-weight","bold")
- .text("Points per Game") 
+ 
  
  var forobj = svg.append("foreignObject")
  .attr("width", 300)
@@ -52,22 +45,15 @@ d3.csv('data.csv',function (data) {
  tabulate(data,columns)
 })
 
-var tabulate = function (data,columns) {
+var tabulateNew = function (data,columns) {
  var margin = {top: 20, right: 10, bottom: 20, left: 10}
  var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
- var svg = d3.select("body").append("svg")
+ var svg = d3.select("reboundpergame").append("svg")
  .attr("width", width + margin.left + margin.right)
  .attr("height", height + margin.top + margin.bottom) 
  .append("g")
  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
- var svgtext = svg.append("text")
- .attr("x",(width/2))
- .attr("y",0-(margin.top/4))
- .style("text-anchor","middle")
- .style("font-size","16px")
- .style("text-decoration","underline")
- .style("font-weight","bold")
- .text("Rebounds per Game") 
+
  
  var forobj = svg.append("foreignObject")
  .attr("width", 300)
@@ -103,26 +89,19 @@ var tabulate = function (data,columns) {
 
 d3.csv('data.csv',function (data) {
 	var columns = ['Name','Team','Position','RPG']
- tabulate(data,columns)
+ tabulateNew(data,columns)
 })
 
 
 var tabulate2 = function (data,columns) {
  var margin = {top: 20, right: 10, bottom: 20, left: 10}
  var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
- var svg = d3.select("#reboundpergame").append("svg")
+ var svg = d3.select("#stealspergame").append("svg")
  .attr("width", width + margin.left + margin.right)
  .attr("height", height + margin.top + margin.bottom) 
  .append("g")
  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
- var svgtext = svg.append("text")
- .attr("x",(width/2))
- .attr("y",0-(margin.top/4))
- .style("text-anchor","middle")
- .style("font-size","16px")
- .style("text-decoration","underline")
- .style("font-weight","bold")
- .text("Steals per Game") 
+
  
  var forobj = svg.append("foreignObject")
  .attr("width", 300)
@@ -165,19 +144,12 @@ d3.csv('data.csv',function (data) {
 var tabulate3 = function (data,columns) {
  var margin = {top: 20, right: 10, bottom: 20, left: 10}
  var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
- var svg = d3.select("#assistpergame").append("svg")
+ var svg = d3.select("#assitspergame").append("svg")
  .attr("width", width + margin.left + margin.right)
  .attr("height", height + margin.top + margin.bottom) 
  .append("g")
  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
- var svgtext = svg.append("text")
- .attr("x",(width/2))
- .attr("y",0-(margin.top/4))
- .style("text-anchor","middle")
- .style("font-size","16px")
- .style("text-decoration","underline")
- .style("font-weight","bold")
- .text("Assists per Game") 
+
  
  var forobj = svg.append("foreignObject")
  .attr("width", 300)
@@ -275,7 +247,7 @@ d3.csv('data.csv',function (data) {
 var tabulate5 = function (data,columns) {
  var margin = {top: 20, right: 10, bottom: 20, left: 10}
  var width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom	
- var svg = d3.select("#stealpergame").append("svg")
+ var svg = d3.select("#stealspergame").append("svg")
  .attr("width", width + margin.left + margin.right)
  .attr("height", height + margin.top + margin.bottom) 
  .append("g")
