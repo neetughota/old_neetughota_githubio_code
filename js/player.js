244e6461f2f2a104ca6040bae17ceefe6d10002f
neetughota <neetu.ghota@gmail.com>
2019-07-29 00:43:29 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -23,6 +23,11 @@ d3.json('Roster.json',function (data) {
        '<div class="row"> <label> College : ' + filteredData[0]["college"] +  'lbs </label> </div> ' +
       '<div class="row"> <label> Draft : ' + filteredData[0].draft.year + ': Round -' + filteredData[0].draft.round + ': Pick -' + filteredData[0].draft.pick + ' </label> </div> ' +
         '</div>'+
+      '<div class="col"> <div class="row"> <label>' + filteredData[0].injury.type +  '</label> </div> ' +
+      '<div class="row"> <label> Games Remaining: ' + filteredData[0].injury.gamesRemaining +  '</label> </div> ' +
+      '<div class="row"> <label> Contract Amt : $' + filteredData[0].contract.amount +  '</label> </div> ' +
+      '<div class="row"> <label> Contract Expire : ' + filteredData[0].contract.exp +  '</label> </div> ' +
+       '</div>'+
       '</div>';
    
    document.getElementById('container').insertAdjacentHTML('afterbegin',html);
