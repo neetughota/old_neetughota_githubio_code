e0015d69e03bfd839e9616fa69433b02706746d5
neetughota <neetu.ghota@gmail.com>
2019-07-28 23:20:55 -0400

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -18,7 +18,8 @@ var svg =  d3.select("#linechart").append("svg").attr("width",960).attr("height"
 var tooltip = d3.select("body").append("div").attr("class", "toolTip");
   
 var x = d3.scaleLinear().range([0, width]);
-var y = d3.scaleBand().range([height, 0]);
+var y = d3.scaleLinear().range([height, 0]);
+var cs = d3.scaleLinear().range([0,width]);
 
 var g = svg.append("g")
 		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -41,7 +42,7 @@ d3.json("Roster.json", function(error, data) {
     g.append("g")
         .attr("class", "x axis")
        	.attr("transform", "translate(0," + height + ")")
-      	.call(d3.axisBottom(x).ticks(5).tickFormat(function(d) { return parseInt(d ); }).tickSizeInner([2]));
+      	.call(d3.axisBottom(x).ticks(10).tickFormat(function(d) { return parseInt(d ); }).tickSizeInner([2]));
 
     g.append("g")
         .attr("class", "y axis")
@@ -60,7 +61,7 @@ d3.json("Roster.json", function(error, data) {
               .style("left", d3.event.pageX - 50 + "px")
               .style("top", d3.event.pageY - 70 + "px")
               .style("display", "inline-block")
-              .html((d.area) + "<br>" + "£" + (d.value));
+              .html((d.Rating) + "<br>" +  + (d.RatingValue));
         })
     		.on("mouseout", function(d){ tooltip.style("display", "none");});
 });
