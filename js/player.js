04a335b0a04da0a427d49a7a2dffd664c499b465
neetughota <neetu.ghota@gmail.com>
2019-07-28 21:04:47 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -10,9 +10,8 @@ function getUrlVars() {
 }
 d3.json('Roster.json',function (data) {
   
-  var filteredData =  data.filter(function(d) {
-          data.players.filter(function(d) {  if( d.name == playerName) {return d};})
-    });
+  var filteredData =  data.players.filter(function(d) {  if( d.name == playerName) {return d};});
+    
 
   var html = '<div class="row" style="margin-right: 15px;margin-left: 15px;">'+
             '<label>' + playerName +  '</label>' +
